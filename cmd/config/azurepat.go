c2e81c5c2814670173a4de88afe40bdc870c295e
cgxarrie <cgxarrie@gmail.com>
2023-03-10 00:32:03 +0100

    add azure pat

--- cmd/config/azurepat.go
+++ cmd/config/azurepat.go
@@ -8,13 +8,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// companyNameCmd represents the companyName command
-var companyNameCmd = &cobra.Command{
-	Use:   "company-name",
-	Short: "set azure company name",
-	Long:  `Set the Azure Conpmany-Name in the configuration file`,
+// azurePATCmd represents the azurePAT command
+var azurePATCmd = &cobra.Command{
+	Use:   "az-pat",
+	Short: "set azure PAT",
+	Long:  `Set the Azure PAT in the configuration file`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := runCompanyNameCmd(args)
+		err := runAzurePATCmd(args)
 		if err != nil {
 			errors.Print(err)
 			return
@@ -23,27 +23,27 @@ var companyNameCmd = &cobra.Command{
 }
 
 func init() {
-	ConfigCmd.AddCommand(companyNameCmd)
+	ConfigCmd.AddCommand(azurePATCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// companyNameCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// azurePATCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// companyNameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// azurePATCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func runCompanyNameCmd(args []string) error {
+func runAzurePATCmd(args []string) error {
 	if len(args) != 1 {
 		return fmt.Errorf("invalid number of arguments")
 	}
 	cfg := appCfg.GetInstance()
 	cfg.Load()
 
-	cfg.Azure.CompanyName = args[0]
+	cfg.Azure.PAT = args[0]
 	err := cfg.Save()
 	if err != nil {
 		return err
